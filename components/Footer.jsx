e995225548616a1a8671a99f2b2d899b74940360
CTianyang <294855805@qq.com>
2024-12-10 18:22:38 +0800

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,21 @@ export default function Footer() {
     <div className="w-full mt-20 text-muted-foreground ">
       <div className="w-full border border-muted-foreground opacity-10 "></div>
       <div className="flex flex-col items-center justify-center h-24 gap-1 font-mono text-sm opacity-70">
+      <p>
+          <a
+            href="https://beian.miit.gov.cn"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline"
+          >
+            冀ICP备2024095384号-1
+          </a>
+        </p>
+
         <p>
           &copy; {new Date().getFullYear()} Ctianyang. All rights reserved.
         </p>
+        
       </div>
     </div>
   );
